05a5ebae2cb7f848b46747c177b23d28232edbb6
Joshua Copeland <Josh@PHPVegas.com>
2016-10-23 10:28:24 -0700

    mind blown

--- resources/views/performer/pay.blade.php
+++ resources/views/performer/pay.blade.php
@@ -1,6 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
+    <style>
+        html, body {
+            background: url({{asset('images/mind-blown.gif')}})  no-repeat center center fixed;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+        }
+        .panel {
+            background-color: rgba(255, 255, 255, 0.9);
+        }
+    </style>
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
@@ -15,11 +27,11 @@
                         @if($paymentId)
                             <h3 class="text-center">Your generosity has blown me away!</h3>
                             <img class="img-responsive center-block" src="{{asset('images/mind-blown.gif')}}">
-                            <div class="text-center"><b>Payment ID:</b> {{ $paymentId }}</div>
+                            <div class="text-center"><b>Transaction ID:</b> {{ $paymentId }}</div>
                         @else
                             <h3>{!! $paymentMessage !!}</h3>
+                            <div class="text-center"><b>Payment Status:</b> {{ $paymentStatus }}</div>
                         @endif
-                        <div class="text-center"><b>Payment Status:</b> {{ $paymentStatus }}</div>
 
                     </div>
                 </div>
